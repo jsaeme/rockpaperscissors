b742487678f9fad761f8f0ea7b92211192c39db0
jsaeme <josefsaeme@gmail.com>
2024-03-19 12:59:00 -0300

    looped game

--- js.js
+++ js.js
@@ -1,28 +1,39 @@
 let y = prompt ('rock, paper, or scissor?').toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
+let game = (playerScore + computerScore);
 
-console.log(getRandomInt(3));
-function getRandomInt(max) {
-    let x = Math.floor(Math.random() * max);
-            if (x === 0 && y.toLowerCase === 'rock') {
-            ai = "It's a tie! Try again!";
-            } else if (x === 0 && y === 'paper') {
-            ai = "You Win! Paper Beats Rock!";
-            } else if (x === 0 && y === 'scissor') {
-            ai = "You lose! Rock Beats Scissor!";
-            } else if (x === 1 && y === 'paper') {
-            ai = "It's a tie! Try again!";
-            } else if (x === 1 && y === 'rock') {
-            ai = "You lose! Paper Beats Rock!";
-            } else if (x === 1 && y === 'scissor') {
-            ai = "You Win! Scissor Beats Paper!";
-            } else if (x === 2 && y === 'rock') {
-            ai = "You Win! Rock Beats Scissors!";
-            } else if (x === 2 && y === 'paper') {
-            ai = "You lose! Scissors Beats Paper!";
-            } else if (x === 2 && y === 'scissor') {
-            ai = "It's a tie! Try again!";
-            } else {
-            ai = "Incorrect input! Try entering either Paper, Rock or Scissor!";
-            }
-        alert (ai);
-  }
+for (game=0;game<6;game++) {
+    console.log(getRandomInt(3));
+    function getRandomInt(max) {
+        let x = Math.floor(Math.random() * max);
+                if (x === 0 && y.toLowerCase === 'rock') {
+                    ai = "It's a tie! Try again!";
+                } else if (x === 0 && y === 'paper') {
+                    playerScore++;
+                    ai = "You Win! Paper Beats Rock!";
+                } else if (x === 0 && y === 'scissor') {
+                    computerScore++;
+                    ai = "You lose! Rock Beats Scissor!";
+                } else if (x === 1 && y === 'paper') {
+                    ai = "It's a tie! Try again!";
+                } else if (x === 1 && y === 'rock') {
+                    computerScore++;
+                    ai = "You lose! Paper Beats Rock!";
+                } else if (x === 1 && y === 'scissor') {
+                    playerScore++;
+                    ai = "You Win! Scissor Beats Paper!";
+                } else if (x === 2 && y === 'rock') {
+                    playerScore++;
+                    ai = "You Win! Rock Beats Scissors!";
+                } else if (x === 2 && y === 'paper') {
+                    computerScore++;
+                    ai = "You lose! Scissors Beats Paper!";
+                } else if (x === 2 && y === 'scissor') {
+                ai = "It's a tie! Try again!";
+                } else {
+                ai = "Incorrect input! Try entering either Paper, Rock or Scissor!";
+                }
+            alert (ai + " " + "player:" + " " + playerScore + " " + "computer:" + " " + computerScore);
+    }
+}
--- js1.js
+++ js1.js
@@ -13,32 +13,30 @@
   }
 
   let y = prompt ('rock, paper, or scissor?').toLowerCase();
-let playerScore = 0;
-let computerScore = 0;
-let gamescore = (playerScore, computerScore)
-console.log(getRandomInt(3));
-function getRandomInt(max) {
-    let x = Math.floor(Math.random() * max);
-            if (x === 0 && y.toLowerCase === 'rock') {
-            ai = "It's a tie! Try again!";
-            } else if (x === 0 && y === 'paper') {
-            ai = ("You Win! Paper Beats Rock!" && playerScore++);
-            } else if (x === 0 && y === 'scissor') {
-            ai = ("You lose! Rock Beats Scissor!" ;
-            } else if (x === 1 && y === 'paper') {
-            ai = "It's a tie! Try again!";
-            } else if (x === 1 && y === 'rock') {
-            ai = "You lose! Paper Beats Rock!";
-            } else if (x === 1 && y === 'scissor') {
-            ai = "You Win! Scissor Beats Paper!";
-            } else if (x === 2 && y === 'rock') {
-            ai = "You Win! Rock Beats Scissors!";
-            } else if (x === 2 && y === 'paper') {
-            ai = "You lose! Scissors Beats Paper!";
-            } else if (x === 2 && y === 'scissor') {
-            ai = "It's a tie! Try again!";
-            } else {
-            ai = "Incorrect input! Try entering either Paper, Rock or Scissor!";
-            }
-        alert (ai && gamescore);
-  }
+
+  console.log(getRandomInt(3));
+  function getRandomInt(max) {
+      let x = Math.floor(Math.random() * max);
+              if (x === 0 && y.toLowerCase === 'rock') {
+              ai = "It's a tie! Try again!";
+              } else if (x === 0 && y === 'paper') {
+              ai = "You Win! Paper Beats Rock!";
+              } else if (x === 0 && y === 'scissor') {
+              ai = "You lose! Rock Beats Scissor!";
+              } else if (x === 1 && y === 'paper') {
+              ai = "It's a tie! Try again!";
+              } else if (x === 1 && y === 'rock') {
+              ai = "You lose! Paper Beats Rock!";
+              } else if (x === 1 && y === 'scissor') {
+              ai = "You Win! Scissor Beats Paper!";
+              } else if (x === 2 && y === 'rock') {
+              ai = "You Win! Rock Beats Scissors!";
+              } else if (x === 2 && y === 'paper') {
+              ai = "You lose! Scissors Beats Paper!";
+              } else if (x === 2 && y === 'scissor') {
+              ai = "It's a tie! Try again!";
+              } else {
+              ai = "Incorrect input! Try entering either Paper, Rock or Scissor!";
+              }
+          alert (ai);
+    }
